331efbf02570b0780c5ee4d1243e0b39359c62e5
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2025-07-11 16:56:58 +0530

    user authorization completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import './db';
 import capsuleRoutes from './routes/capsule';
 import registerRoutes from './routes/register';
-
+import loginRoutes from './routes/login';
 const app = express();
 const PORT = 3000;
 
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use('/register', registerRoutes); // register yourself route
 app.use('/capsules', capsuleRoutes); // register your capsule route
-
+app.use('/login', loginRoutes); // login 
 app.get('/', (req, res) => {
   res.send('Server running!');
 });
